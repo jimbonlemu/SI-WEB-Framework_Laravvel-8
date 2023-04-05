a832160430b6ffd5fabf4059353c6928607a60f3
perompak_kode <iqbaltech29@gmail.com>
2023-04-06 01:38:21 +0700

    adding models and calls the models on routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,54 +30,24 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Iqbal Maulana",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore illum suscipit mollitia veritatis. Enim tempore accusantium nostrum deserunt. Esse, distinctio, soluta cum tempore dicta provident, nobis deleniti sequi dolor quod modi libero sapiente quidem autem! Odit maiores sapiente fugiat repellat voluptate, maxime soluta reiciendis aperiam ipsa architecto! Iure tempora quod reiciendis et. Vero sapiente debitis fugit nihil repellendus accusamus laborum eveniet velit voluptas? Sequi, ipsam dolorem. Quis laborum similique quod sapiente illum quas obcaecati quia dolores totam inventore aspernatur est itaque enim rem sequi expedita impedit, ab dolore officia deleniti!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Roni Hitam",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, facilis. Numquam sit ipsa, et soluta quidem tenetur. Asperiores reiciendis voluptatum pariatur, iusto quas fugit molestiae, aperiam minus vero exercitationem ipsum."
-        ]
-    ];
-
-    return view('post', [
+    return view('post', 
+    [
         "title" => "Blog",
-        "post" => $blog_posts
+        "post" => Post::all()
     ]);
 });
 
 Route::get('post/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Iqbal Maulana",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore illum suscipit mollitia veritatis. Enim tempore accusantium nostrum deserunt. Esse, distinctio, soluta cum tempore dicta provident, nobis deleniti sequi dolor quod modi libero sapiente quidem autem! Odit maiores sapiente fugiat repellat voluptate, maxime soluta reiciendis aperiam ipsa architecto! Iure tempora quod reiciendis et. Vero sapiente debitis fugit nihil repellendus accusamus laborum eveniet velit voluptas? Sequi, ipsam dolorem. Quis laborum similique quod sapiente illum quas obcaecati quia dolores totam inventore aspernatur est itaque enim rem sequi expedita impedit, ab dolore officia deleniti!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Roni Hitam",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, facilis. Numquam sit ipsa, et soluta quidem tenetur. Asperiores reiciendis voluptatum pariatur, iusto quas fugit molestiae, aperiam minus vero exercitationem ipsum."
-        ]
-    ];
+    // $new_post =[];
 
-    $new_post =[];
-
-    foreach ($blog_posts as $posted) {
-        if ($posted["slug"] === $slug) {
-            $new_post = $posted;
-            # code...
-        }
-        # code...
-    }
+    // foreach ($blog_posts as $posted) {
+    //     if ($posted["slug"] === $slug) {
+    //         $new_post = $posted;
+    //     }
+    // }
     return view('posts', [
         "title" => "Single Post",
-        "post" => $new_post
+        "post" => Post::find($slug)
+
     ]);
 });
